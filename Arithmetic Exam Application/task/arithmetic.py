8f53740707b014908278654e0a930a56b6eee3c4
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-01-27 23:18:34 +0500

    Completed 1 stage: Implemented basic operations

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -1,1 +1,10 @@
 # write your code here
+x, operator, y = input().split()
+if operator == '+':
+    print(int(x) + int(y))
+elif operator == '-':
+    print(int(x) - int(y))
+elif operator == '*':
+    print(int(x) * int(y))
+elif operator == '/':
+    print(int(x) / int(y))
