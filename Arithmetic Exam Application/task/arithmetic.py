55c7203a639125a4e59ea04c553dcaf57cab8bfd
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-01-28 12:03:08 +0500

    Completed 2 stage: Implemented tasks

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -1,10 +1,26 @@
-# write your code here
-x, operator, y = input().split()
-if operator == '+':
-    print(int(x) + int(y))
-elif operator == '-':
-    print(int(x) - int(y))
-elif operator == '*':
-    print(int(x) * int(y))
-elif operator == '/':
-    print(int(x) / int(y))
+import random
+
+
+def evaluate(x, operator, y):
+    if operator == '+':
+        result = x + y
+    elif operator == '-':
+        result = x - y
+    else:
+        result = x * y
+    return result
+
+
+def check(result, user_input):
+    if result == int(user_input):
+        print('Right!')
+    else:
+        print('Wrong!')
+
+
+first_number = random.randint(2, 9)
+second_number = random.randint(2, 9)
+oper = random.choice(['+', '-', '*'])
+
+print(first_number, oper, second_number)
+check(evaluate(first_number, oper, second_number), input())
