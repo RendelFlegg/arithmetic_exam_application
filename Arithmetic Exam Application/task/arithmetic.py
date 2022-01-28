d996ff5a148c28d03b2dfbf7eec3fb006d4b04aa
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-01-28 16:36:07 +0500

    Completed stage 3: Implemented input check

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -11,16 +11,38 @@ def evaluate(x, operator, y):
     return result
 
 
-def check(result, user_input):
-    if result == int(user_input):
+def get_correct_answer():
+    first_number = random.randint(2, 9)
+    second_number = random.randint(2, 9)
+    oper = random.choice(['+', '-', '*'])
+    print(first_number, oper, second_number)
+    return evaluate(first_number, oper, second_number)
+
+
+def check_result(result, answer):
+    global score
+    if result == answer:
         print('Right!')
+        score += 1
+        return True
     else:
         print('Wrong!')
+        return False
 
 
-first_number = random.randint(2, 9)
-second_number = random.randint(2, 9)
-oper = random.choice(['+', '-', '*'])
+def task():
+    correct_answer = get_correct_answer()
+    while True:
+        try:
+            user_answer = int(input())
+            check_result(correct_answer, user_answer)
+            break
+        except ValueError:
+            print('Incorrect format.')
 
-print(first_number, oper, second_number)
-check(evaluate(first_number, oper, second_number), input())
+
+score = 0
+
+for _ in range(5):
+    task()
+print(f'Your mark is {score}/5')
